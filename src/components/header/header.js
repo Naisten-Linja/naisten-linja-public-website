a91c2266089c0f9d948c6f3966b4af9238540768
nathandao <nathan@guynathan.com>
2021-02-22 16:28:17 +0200

    Add styling for desktop menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,33 +6,22 @@ import Menu from '../menu/menu';
 
 const Header = () => (
   <header>
-    <div className="layout-container">
-      <div className="row menu-row align-space-between">
-        <nav className="col">
-          <Menu />
-        </nav>
-        <div className="col">
-          {/* in future there's gonna be the language switch here */}
-        </div>
-        <div className="col">
-          <a className="escape-link-box" href="https://google.com">
-            <span className="escape-link">Poistu sivuilta nopeasti</span>
-          </a>
-        </div>
-      </div>
-      <div className="flex-column">
-        <Link className="logo-link" to="/">
-          <img
-            src="https://images.ctfassets.net/pbxhiiewat8h/3R7iRMIYcL0a2o8rWeQtgl/7bdfd19edfdc95c73cedff7c244f8edf/mustapyorealogo_2.svg"
-            alt="Naisten Linja"
-          />
-        </Link>
-        <h2>
-          On the women's side.
-          <br />
-          Against violence.
-        </h2>
-      </div>
+    <Menu />
+    <a className="escape-link-box" href="https://google.com">
+      <span className="escape-link">Poistu sivuilta nopeasti</span>
+    </a>
+    <div className="flex-column">
+      <Link className="logo-link" to="/">
+        <img
+          src="https://images.ctfassets.net/pbxhiiewat8h/3R7iRMIYcL0a2o8rWeQtgl/7bdfd19edfdc95c73cedff7c244f8edf/mustapyorealogo_2.svg"
+          alt="Naisten Linja"
+        />
+      </Link>
+      <h2>
+        On the women's side.
+        <br />
+        Against violence.
+      </h2>
     </div>
   </header>
 );
--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -45,66 +45,96 @@ const Menu = () => {
 
   return (
     <nav className="MainMenu">
-      <input className="menu-btn" type="checkbox" id="menu-btn" />
-      <label className="menu-icon" htmlFor="menu-btn">
-        <span className="nav-icon" role="img">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="32"
-            viewBox="0 0 24 24"
-            width="32"
-          >
-            <path d="M0 0h24v24H0z" fill="none" />
-            <path
-              fill="white"
-              d="M3 15h18v-2H3v2zm0 4h18v-2H3v2zm0-8h18V9H3v2zm0-6v2h18V5H3z"
-            />
-          </svg>
-        </span>
-      </label>
-      <ul className="menu">
-        {topLevelPages.map((topLevelPage, i) => (
-          <li key={i}>
-            <Link
-              to={
-                topLevelPage.menuPage.slug === 'etusivu'
-                  ? '/'
-                  : topLevelPage.menuPage.slug
-              }
+      <div className="MainMenu__container">
+        <input className="menu-btn" type="checkbox" id="menu-btn" />
+        <label className="menu-icon" htmlFor="menu-btn">
+          <span className="nav-icon" role="img">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="32"
+              viewBox="0 0 24 24"
+              width="32"
             >
-              {topLevelPage.pageContainerName !== null
-                ? topLevelPage.pageContainerName
-                : topLevelPage.menuPage.pageName}
-            </Link>
-            {topLevelPage.menuPageSubpages !== null && (
-              <ul>
-                {topLevelPage.menuPageSubpages.map((firstNavigationPage, i) => (
-                  <li key={i}>
-                    <Link to={firstNavigationPage.menuPage.slug}>
-                      {firstNavigationPage.pageContainerName !== null
-                        ? firstNavigationPage.pageContainerName
-                        : firstNavigationPage.menuPage.pageName}
-                    </Link>
-                    {firstNavigationPage.menuPageSubpages !== null && (
-                      <ul>
-                        {firstNavigationPage.menuPageSubpages.map(
-                          (secondNavigationPage, i) => (
-                            <li key={i}>
-                              <Link to={secondNavigationPage.menuPage.slug}>
-                                {secondNavigationPage.menuPage.pageName}
-                              </Link>
-                            </li>
-                          ),
-                        )}
-                      </ul>
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path
+                fill="white"
+                d="M3 15h18v-2H3v2zm0 4h18v-2H3v2zm0-8h18V9H3v2zm0-6v2h18V5H3z"
+              />
+            </svg>
+          </span>
+        </label>
+        <ul className="menu">
+          {topLevelPages.map((topLevelPage, i) => (
+            <li key={i}>
+              <Link
+                to={
+                  topLevelPage.menuPage.slug === 'etusivu'
+                    ? '/'
+                    : topLevelPage.menuPage.slug
+                }
+              >
+                {topLevelPage.pageContainerName !== null
+                  ? topLevelPage.pageContainerName
+                  : topLevelPage.menuPage.pageName}
+              </Link>
+              {topLevelPage.menuPageSubpages !== null && (
+                <div className="MainMenu__submenu-container">
+                  <ul>
+                    {topLevelPage.menuPageSubpages.map(
+                      (firstNavigationPage, i) => (
+                        <li key={i}>
+                          <Link to={firstNavigationPage.menuPage.slug}>
+                            {firstNavigationPage.pageContainerName !== null
+                              ? firstNavigationPage.pageContainerName
+                              : firstNavigationPage.menuPage.pageName}
+                          </Link>
+                          {firstNavigationPage.menuPageSubpages !== null && (
+                            <ul>
+                              {firstNavigationPage.menuPageSubpages.map(
+                                (secondNavigationPage, i) => (
+                                  <li key={i}>
+                                    <Link
+                                      to={secondNavigationPage.menuPage.slug}
+                                    >
+                                      {secondNavigationPage.menuPage.pageName}
+                                    </Link>
+                                    {secondNavigationPage.menuPageSubpages !==
+                                      null && (
+                                      <ul>
+                                        {secondNavigationPage.menuPageSubpages.map(
+                                          (thirdNavigationPage, i) => (
+                                            <li key={i}>
+                                              <Link
+                                                to={
+                                                  thirdNavigationPage.menuPage
+                                                    .slug
+                                                }
+                                              >
+                                                {
+                                                  thirdNavigationPage.menuPage
+                                                    .pageName
+                                                }
+                                              </Link>
+                                            </li>
+                                          ),
+                                        )}
+                                      </ul>
+                                    )}
+                                  </li>
+                                ),
+                              )}
+                            </ul>
+                          )}
+                        </li>
+                      ),
                     )}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
-      </ul>
+                  </ul>
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
