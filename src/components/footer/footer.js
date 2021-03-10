ac570a4edfb08c6f954bc14a12b1c0b8e225b631
nathandao <nathan@guynathan.com>
2021-03-10 16:12:40 +0200

    Update footer stylings & font size in service & content boxes

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,36 +2,34 @@ import { Link } from 'gatsby';
 // import PropTypes from 'prop-types';
 import React from 'react';
 
-const Footer = () => (
-  <footer>
-    <div className="layout-container">
-      <div className="col-sm-12">
-        <h3>Follow us on social media</h3>
-        <ul>
-          <li>
-            <a href="https://www.instagram.com/naistenlinja">Instagram</a>
-          </li>
-          <li>
-            <a href="https://www.facebook.com/naistenlinjasuomessa">Facebook</a>
-          </li>
-          <li>
-            <a href="https://twitter.com/naistenlinja">Twitter</a>
-          </li>
-          <li>
-            <a href="https://www.youtube.com/channel/UCWuV35u6rHGGbfO_qsWes2w">
-              Youtube
-            </a>
-          </li>
-        </ul>
-        <hr className="breakline" />
-        <Link className="company-information" to="/yhteystiedot">
-          Yhteystiedot
-        </Link>
+import './footer.scss';
 
-        <div className="copyright-info">
-          © {new Date().getFullYear()} Naisten Linja Suomessa ry
-        </div>
-      </div>
+const Footer = () => (
+  <footer className="layout-container">
+    <h3>Follow us on social media</h3>
+    <ul>
+      <li>
+        <a href="https://www.instagram.com/naistenlinja">Instagram</a>
+      </li>
+      <li>
+        <a href="https://www.facebook.com/naistenlinjasuomessa">Facebook</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/naistenlinja">Twitter</a>
+      </li>
+      <li>
+        <a href="https://www.youtube.com/channel/UCWuV35u6rHGGbfO_qsWes2w">
+          Youtube
+        </a>
+      </li>
+    </ul>
+    <hr className="breakline" />
+    <Link className="company-information" to="/yhteystiedot">
+      Yhteystiedot
+    </Link>
+
+    <div className="copyright-info">
+      © {new Date().getFullYear()} Naisten Linja Suomessa ry
     </div>
   </footer>
 );
