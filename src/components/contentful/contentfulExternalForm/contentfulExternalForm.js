d3189074c6febfa82d318ca1780ed9e0016eb136
nathandao <nathan@guynathan.com>
2021-03-23 06:42:04 +0200

    Update external form appearance to be more accessible

--- src/components/contentful/contentfulExternalForm/contentfulExternalForm.js
+++ src/components/contentful/contentfulExternalForm/contentfulExternalForm.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import './contentfulExternalForm.scss';
+import FormIcon from '../../icons/form';
+import ExternalLinkIcon from '../../icons/externalLink';
 
 const ContentfulExternalForm = ({ content }) => {
   const { linkText, formUrl, openInNewTab } = content;
@@ -11,7 +13,24 @@ const ContentfulExternalForm = ({ content }) => {
         href={formUrl}
         {...(openInNewTab ? { target: '_blank' } : {})}
       >
+        <FormIcon
+          color="white"
+          aria-hidden="true"
+          width={32}
+          height={32}
+          className="form-icon"
+        />
+
         {linkText}
+
+        {openInNewTab && (
+          <ExternalLinkIcon
+            width={20}
+            height={20}
+            aria-label="Link opens in a new tab"
+            className="external-link-icon"
+          />
+        )}
       </a>
     </section>
   );
