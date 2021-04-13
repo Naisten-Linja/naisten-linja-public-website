55151a4433ca5e31a8fbea8561bebcc2a5683f43
nathandao <nathan@guynathan.com>
2021-04-13 05:00:42 +0300

    Add social media icons

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,24 +1,38 @@
 import { Link } from 'gatsby';
-// import PropTypes from 'prop-types';
 import React from 'react';
+
+import FacebookIcon from '../icons/facebook';
+import TwitterIcon from '../icons/twitter';
+import InstagramIcon from '../icons/instagram';
+import YoutubeIcon from '../icons/youtube';
 
 import './footer.scss';
 
 const Footer = () => (
   <footer className="layout-container">
     <h3>Follow us on social media</h3>
-    <ul>
+    <ul className="social-media-links">
       <li>
-        <a href="https://www.instagram.com/naistenlinja">Instagram</a>
+        <a href="https://www.instagram.com/naistenlinja">
+          <InstagramIcon />
+          Instagram
+        </a>
       </li>
       <li>
-        <a href="https://www.facebook.com/naistenlinjasuomessa">Facebook</a>
+        <a href="https://www.facebook.com/naistenlinjasuomessa">
+          <FacebookIcon />
+          Facebook
+        </a>
       </li>
       <li>
-        <a href="https://twitter.com/naistenlinja">Twitter</a>
+        <a href="https://twitter.com/naistenlinja">
+          <TwitterIcon />
+          Twitter
+        </a>
       </li>
       <li>
         <a href="https://www.youtube.com/channel/UCWuV35u6rHGGbfO_qsWes2w">
+          <YoutubeIcon />
           Youtube
         </a>
       </li>
