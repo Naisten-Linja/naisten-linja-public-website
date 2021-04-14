866bb1ef7f566524f300884784c843b4ac33d7fd
nathandao <nathan@guynathan.com>
2021-04-14 07:47:24 +0300

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,25 @@
-import React from "react"
+import React from 'react';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section className="full-width-section">
+      <div className="layout-container">
+        <h1>Sivua ei löytynyt</h1>
+        <div>
+          <p>
+            Sen nimi on ehkä kirjoitettu väärin, tai on tapahtunut virhe.{' '}
+            <a href="https://link.webropolsurveys.com/Participation/Public/abb56125-ea90-42bb-a623-3fdef155bbcc?displayId=Fin2184615">
+              Voit jättää meille palautteen.
+            </a>
+          </p>
+        </div>
+      </div>
+    </section>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
