878da2c587f141da6f8bf31af97efcff724a1390
Eeva-Jonna Panula <eeva-jonna.panula@futurice.com>
2021-10-02 07:28:17 +0300

    Use strict equality with cookie banner's checks

--- src/components/cookieBanner/cookieBanner.js
+++ src/components/cookieBanner/cookieBanner.js
@@ -57,11 +57,11 @@ export function getConsentCookie() {
   const decodedCookie = decodeURIComponent(window.document.cookie);
   const ca = decodedCookie.split(';');
   for (var i = 0; i < ca.length; i++) {
-    const c = ca[i];
-    while (c.charAt(0) == ' ') {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
       c = c.substring(1);
     }
-    if (c.indexOf(name) == 0) {
+    if (c.indexOf(name) === 0) {
       return c.substring(name.length, c.length);
     }
   }
