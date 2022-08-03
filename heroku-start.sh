13cb6afa312ae8a7d7fd409168b9b2a85da7e612
Tuomas Karjalainen <tuomas.karjalainen@futurice.com>
2022-08-03 22:23:54 +0300

    Run gatsby dev in fixed internal port in Heroku preview

--- heroku-start.sh
+++ heroku-start.sh
@@ -5,7 +5,7 @@ then # DEV/PREVIEW Heroku app
     # Runs two commands simultaneously
     # source: https://help.heroku.com/CTFS2TJK/how-do-i-run-multiple-processes-on-a-dyno
     trap '' SIGTERM;
-    npm run develop & # run gatsby dev server for serving a preview with fast reload
+    npm run start & # run gatsby dev server for serving a preview with fast reload
     bin/boot & # start nginx from heroku-buildpack-static
     wait -n;
     kill -SIGTERM -$$;
