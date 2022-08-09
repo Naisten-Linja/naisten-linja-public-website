06a0e7a740c8325a9bfe13e1c9d8e80bfd27005d
Tuomas Karjalainen <tuomas.karjalainen@futurice.com>
2022-08-09 09:43:36 +0300

    wip: add .htpasswd generation from env

--- heroku-start.sh
+++ heroku-start.sh
@@ -2,6 +2,7 @@
 
 if [ "$NODE_ENV" == "development" ]
 then # DEV/PREVIEW Heroku app
+    config/make-htpasswd
     bin/start-nginx npm run start
 else # PRODUCTION Heroku app
     bin/start-nginx-solo
