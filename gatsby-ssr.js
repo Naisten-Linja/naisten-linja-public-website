1929a195648652f5f52fe8b48e8c72c9084f39ae
Tuomas Karjalainen <tuomas.karjalainen@futurice.com>
2022-08-11 16:10:12 +0300

    Add facebook verification tag

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { withPrefix } from 'gatsby';
 
-export const onRenderBody = ({ setPostBodyComponents }) => {
+export const onRenderBody = ({ setPostBodyComponents, setHeadComponents }) => {
+  setHeadComponents([
+    <meta
+      key="facebook-domain-verification"
+      name="facebook-domain-verification"
+      content="8lnrf1d0ch8i4fqblko7e0iibhafpc"
+    />
+  ]);
   setPostBodyComponents([
     <script
       key="ninchat"
