bac607b5b13b03a7026275dbdb33c0817aa5d617
Pietari Eronen <pietari.eronen@nitro.fi>
2024-12-04 11:44:58 +0200

    AlertBar scroll transition + UI fixes

--- src/components/ui/AlertBar/AlertBar.tsx
+++ src/components/ui/AlertBar/AlertBar.tsx
@@ -2,20 +2,50 @@ import React from 'react';
 import Container from '../utils/Container/Container';
 import './AlertBar.scss';
 import { Link } from 'gatsby';
-// import { IoArrowForwardSharp } from 'react-icons/io5';
+import { IoArrowForwardSharp } from 'react-icons/io5';
 
 const AlertBar = ({ alertText, alertLink }) => {
   return (
-    <Container theme={'alert'} background={true} size={'xx-large'}>
+    <div className="AlertBar_container">
       {alertLink ? (
-        <Link to={`/${alertLink}`} className="AlertBar_link_wrapper">
-          <p className="AlertBar_container_link">{alertText}</p>
-          {/* <IoArrowForwardSharp /> */}
-        </Link>
+        <>
+          <Link to={`/${alertLink}`} className="AlertBar_link">
+            <p className="AlertBar_content">
+              {alertText} <IoArrowForwardSharp />
+            </p>
+          </Link>
+          <Link
+            aria-hidden={true}
+            to={`/${alertLink}`}
+            className="AlertBar_link"
+          >
+            <p className="AlertBar_content">
+              {alertText} <IoArrowForwardSharp />
+            </p>
+          </Link>
+          <Link
+            aria-hidden={true}
+            to={`/${alertLink}`}
+            className="AlertBar_link"
+          >
+            <p className="AlertBar_content">
+              {alertText} <IoArrowForwardSharp />
+            </p>
+          </Link>
+          <Link
+            aria-hidden={true}
+            to={`/${alertLink}`}
+            className="AlertBar_link"
+          >
+            <p className="AlertBar_content">
+              {alertText} <IoArrowForwardSharp />
+            </p>
+          </Link>
+        </>
       ) : (
-        <p className="AlertBar_container">{alertText}</p>
+        <p className="AlertBar_text">{alertText}</p>
       )}
-    </Container>
+    </div>
   );
 };
 
