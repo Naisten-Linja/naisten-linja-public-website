21f4caf431e692711968244bea5784a0610e4e9c
PepitaKautto <pepita.kautto@nitro.fi>
2025-03-04 15:21:47 +0200

    Fix: 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,23 +2,35 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import Seo from '../components/seo';
+import Container from '../components/ui/utils/Container/Container';
 
 const NotFoundPage = () => (
-  <Layout lang="fi">
+  <Layout
+    lang="fi"
+    hero={{
+      heroTitle: 'Sivua ei löytynyt',
+      backLink: { slug: '', pageName: 'Etusivu' },
+    }}
+  >
     <Seo title="Sivua ei löytynyt" />
-    <section className="full-width-section">
-      <div className="layout-container">
-        <h1>Sivua ei löytynyt</h1>
-        <div>
-          <p>
-            Sen nimi on ehkä kirjoitettu väärin, tai on tapahtunut virhe.{' '}
-            <a href="https://link.webropolsurveys.com/Participation/Public/abb56125-ea90-42bb-a623-3fdef155bbcc?displayId=Fin2184615">
-              Voit jättää meille palautteen.
-            </a>
-          </p>
+    <Container>
+      <section className="full-width-section">
+        <div className="layout-container">
+          <h1>Sivua ei löytynyt</h1>
+          <div>
+            <p>
+              <br />
+              Sen nimi on ehkä kirjoitettu väärin, tai on tapahtunut virhe.{' '}
+              <br />
+              <br />
+              <a href="https://link.webropolsurveys.com/Participation/Public/abb56125-ea90-42bb-a623-3fdef155bbcc?displayId=Fin2184615">
+                Voit jättää meille palautteen painamalla tästä.
+              </a>
+            </p>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Container>
   </Layout>
 );
 
