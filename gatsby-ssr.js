5d4cb0e8c0af5dd49b4534fc5bfbe11909c7fd96
PepitaKautto <pepita.kautto@nitro.fi>
2025-04-30 15:36:56 +0300

    fix: iRaiser form

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -16,10 +16,6 @@ export const onRenderBody = ({ setPostBodyComponents, setHeadComponents }) => {
       charSet="UTF-8"
       type="text/javascript"
     />,
-    <script
-      src="https://lahjoita.naistenlinja.fi/libs.iraiser.eu/libs/payment/frame/1.5/IRaiserFrame.js"
-      type="text/plain"
-      data-cookieconsent="ignore"
-    />,
+    <script src="https://lahjoita.naistenlinja.fi/libs.iraiser.eu/libs/payment/frame/1.5/IRaiserFrame.js" />,
   ]);
 };
